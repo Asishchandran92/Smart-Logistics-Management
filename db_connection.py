b0900ab2db623ab0926781c2cf168efcdf4ba77a
Asishchandran92 <asishc0392@gmail.com>
2026-03-17 20:31:05 +0530

    Add files via upload

---
+++ db_connection.py
@@ -0,0 +1,18 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+def get_connection():
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="root123",
+            database="smart_logistics"
+        )
+
+        return connection
+
+    except Error as e:
+        print("Database connection error:", e)
+        return None
