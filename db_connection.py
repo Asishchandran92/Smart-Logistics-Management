e1adcef8a07220fd6c328e9172e5c2208ee40167
Asishchandran92 <asishc0392@gmail.com>
2026-03-18 17:32:25 +0530

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -1,11 +1,16 @@
 import mysql.connector
+from mysql.connector import Error
 
 def get_connection():
     try:
         connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="root123",
-            database="smart_logistics"
+          host="localhost",
+          user="root",
+          password="root123",
+          database="smart_logistics"
         )
+
         return connection
+    except Error as e:
+        print("Database connection error:", e)
+        return None
